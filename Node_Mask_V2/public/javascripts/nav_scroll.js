9b127bc3c14d033e01c7f43e4c5f7d53243d208e
callor88 <callor88@naver.com>
2020-03-14 04:27:55 +0900

    mask_response

---
+++ Node_Mask_V2/public/javascripts/nav_scroll.js
@@ -0,0 +1,15 @@
+$(function(){
+    let preScroll = $(window).scrollTop()
+    $(window).scroll(function(e){
+
+        let curScroll = $(window).scrollTop()
+        /*
+        if(preScroll > curScroll) {
+            $("ul.main-menu").css("top",0)
+        } else {
+            $("ul.main-menu").css("top",-100)
+        }
+        */
+        preScroll = curScroll
+    })
+})
---
+++ Node_Mask_V2/routes/tmap_sec.js
@@ -0,0 +1,4 @@
+var tmap = {
+    api_key : "l7xxb7d6801cb80a4bd281347d9f0ac404c0"
+}
+module.exports = tmap
