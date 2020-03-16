ff5ce77e66a85678bf95e722d5ef2fd6691792dc
callor <callor@callor.com>
2020-03-16 15:41:33 +0900

    naver2

---
+++ Node_Naver_V2/config/naver_secret_sample.js
@@ -0,0 +1,9 @@
+
+var naver = {
+
+    client_id : "YOUR NAVER CLIENT ID",
+    client_secret : "YOUR NAVER CLIENT SEC",
+    movie_url : "https://openapi.naver.com/v1/search/movie.json",
+    book_url : "https://openapi.naver.com/v1/search/book.json"
+
+}
