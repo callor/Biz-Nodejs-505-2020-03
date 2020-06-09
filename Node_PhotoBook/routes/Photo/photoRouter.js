932b490303b79ca89e32070e647fcb74f57e2017
callor <callor@callor.com>
2020-06-09 15:00:19 +0900

    photo

--- Node_PhotoBook/routes/Photo/photoRouter.js
+++ Node_PhotoBook/routes/Photo/photoRouter.js
@@ -1,14 +1,16 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
+var fs = require("fs");
 var photoVO = require("../../models/photoBook");
+var photo_dir = path.join(__dirname, "/../../", "public", "images");
 
 // 파일 업로드를 도와줄 middleware
 var multer = require("multer");
 
 var saveFiles = multer.diskStorage({
   destination: (req, file, func) => {
-    let photo_dir = path.join(__dirname, "/../../", "public", "images");
+    // let photo_dir = path.join(__dirname, "/../../", "public", "images");
     func(null, photo_dir);
   },
   filename: (req, file, func) => {
@@ -46,6 +48,7 @@ router.post("/upload", function (req, res) {
       // vo.save() method만 호출하면 DB에 4개의 변수가 모두
       // 저장이된다.
       vo.save(function (err, data) {
+        console.log(err, data);
         res.redirect("/");
       });
     }
@@ -55,14 +58,74 @@ router.post("/upload", function (req, res) {
 });
 
 router.get("/upload", function (req, res) {
-  res.render("photo/upload");
+  let vo = new photoVO();
+  res.render("photo/upload", { photo: vo });
 });
 
 router.get("/", function (req, res) {
   // render() method는 view 파일과 데이터를
   // rendering 하여 web browser에게
   // html로 전송하는 method
-  res.render("photo/list");
+  photoVO.find({}).exec(function (err, data) {
+    res.render("photo/list", { photos: data });
+  });
+});
+
+router.get("/view/:id", function (req, res) {
+  let id = req.params.id;
+  photoVO.findOne({ _id: id }).exec(function (err, data) {
+    res.render("photo/view", { photo: data });
+  });
+});
+
+router.get("/delete/:id", function (req, res) {
+  let id = req.params.id;
+  photoVO.findByIdAndDelete({ _id: id }).exec(function (err, data) {
+    let del_file = path.join(photo_dir, data.pPhotoName);
+    fs.unlinkSync(del_file);
+    res.redirect("/");
+  });
+});
+
+router.get("/update/:id", function (req, res) {
+  let id = req.params.id;
+  photoVO.findOne({ _id: id }).exec(function (err, data) {
+    res.render("photo/upload", { photo: data });
+  });
+});
+
+router.post("/update/:id", function (req, res) {
+  saveFile(req, res, function (error) {
+    let id = req.params.id;
+    // $set : mongo Keyword
+    // table의 모든칼럼에 일치하는 내용이 있으면
+    // update를 수행하라
+    // _id = id와 일치하는 데이터가 있으면
+
+    if (req.file) {
+      let originalname = req.file.originalname;
+      let photoname = req.file.filename;
+
+      req.body.pOriginalName = originalname;
+      req.body.pPhotoName = photoname;
+    }
+
+    // update되기 이전의 데이터를 exec()함수의 data부분에 임시저장하고
+    // update를 수행한 후에 해당 데이터를 참조할수 있도록
+    // 남겨놓은 함수
+    photoVO
+      .findByIdAndUpdate({ _id: id }, { $set: req.body })
+      .exec(function (err, data) {
+        if (req.file) {
+          // 파일 삭제코드 추가
+          let del_file = path.join(photo_dir, data.pPhotoName);
+          // 파일삭제를 하기 위한 코드
+          fs.unlinkSync(del_file);
+        }
+        console.log(data);
+        res.redirect("/photo/view/" + id);
+      });
+  });
 });
 
 module.exports = router;
